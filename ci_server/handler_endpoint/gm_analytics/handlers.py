7ccef8605d5c9cb5b8de6df7270984bcee10574a
juanswan13 <juanswan13@hotmail.com>
2018-11-16 15:39:02 -0500

    parcial images

--- ci_server/handler_endpoint/gm_analytics/handlers.py
+++ ci_server/handler_endpoint/gm_analytics/handlers.py
@@ -3,10 +3,14 @@ import logging
 import requests
 import json
 import docker
-from flask import request
+from flask import Flask, request, json
+
+def hello():
+    result = {'command_return': 'Hello'}
+    return result
 
 def repository_merged():
-    logging.debug('Event Received')
+    retorno = ""
     post_json_data = request.get_data()
     string = str(post_json_data, 'utf-8')
     jsonFile = json.loads(string)
@@ -17,7 +21,6 @@ def repository_merged():
         json_image_url = 'https://raw.githubusercontent.com/juanswan13/sd2018b-exam2/' + pull_id + '/images.json'
         json_image_response = requests.get(json_image_url)
         images_json = json.loads(json_image_response.content)
-
         for service in images_json["images"]:
             service_name = service["service_name"]
             image_type = service["type"]
@@ -29,13 +32,11 @@ def repository_merged():
                 file.write(str(file_response.content, 'utf-8'))
                 file.close()
                 image_tag = domain + '/' + service_name + ':' + image_version
-
                 client = docker.from_env()
                 client.images.build(path="./", tag=image_tag)
                 client.images.push(image_tag)
                 client.images.remove(image=image_tag, force=True)
-                result = {'command_return': image_tag}
-
+                retorno = image_tag + " - " + retorno
             elif image_type == 'AMI':
                 image_url = 'https://raw.githubusercontent.com/juanswan13/sd2018b-exam2/' + pull_id + '/' + service_name + '/AMI'
                 file_response=requests.get(image_url)
@@ -43,11 +44,11 @@ def repository_merged():
                 file.write(str(file_response.content, 'utf-8'))
                 file.close()
                 image_tag = domain + '/' + service_name + ':' + image_version
+
+                retorno = image_tag + " - " + retorno
             else:
                 result = {'command_return': 'ERROR: JSON does not have a correct format.'}
-
-        result = {'command_return': ''}
+        result = {'command_return': retorno}
     else:
         result = {'command_return': 'Pull request was not merged'}
-
     return result
